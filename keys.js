8d6a9c29d32225c168c2ee66f8aa2208c448bb7d
justin deguzman <justindeguzman@justins-mbp.attlocal.net>
2017-10-16 23:06:55 -0700

    secondsecondPush

--- keys.js
+++ keys.js
@@ -1,11 +1,26 @@
-// console.log('this is loaded');
+// console.log('this is loaded'); // 
 
 var twitterKeys = {
   consumer_key: 'RFzLU4CYNfXHy24KrQ0lq0Dax',
   consumer_secret: 'v76ZYUKM42r9HILL6csElxPYEe6HOXWqTWDEjnERQPpavor1Xy',
   access_token_key: '918663565459910656-b0XscaXG6VIzMBIAM00HLGCUlO9psZX',
-  access_token_secret: '22lrl1fKQMG0amuZOJ0sVskDUORAkWUbj33KildLWFZm2',
+  access_token_secret: '22lrl1fKQMG0amuZOJ0sVskDUORAkWUbj33KildLWFZm2'
 }
 
 module.exports = twitterKeys;
 
+/*
+
+var twitterTimeline = require('twitter-timeline-middleware');  // <--- this is the one that goes in liri.js
+var twitterCredentials = {
+  consumer_key: 'RFzLU4CYNfXHy24KrQ0lq0Dax',
+  consumer_secret: 'v76ZYUKM42r9HILL6csElxPYEe6HOXWqTWDEjnERQPpavor1Xy',
+  access_token_key: '918663565459910656-b0XscaXG6VIzMBIAM00HLGCUlO9psZX',
+  access_token_secret: '22lrl1fKQMG0amuZOJ0sVskDUORAkWUbj33KildLWFZm2'  // copied all this to Here, 12:42am made edits to key names To include Underscore(s)
+};
+ 
+// i Dont think we need This: app.use('/timeline/:iosbigfaced2', twitterTimeline(twitterCredentials));   // copied from liri-.js
+
+module.exports = twitterTimeline;
+
+*/
--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@ var twitterKeys = require('./keys.js');
 
 // console.log(twitterKeys);
 
+
 // console.log(process.argv[2]);
 
 
@@ -16,9 +17,58 @@ var Twitter = require('twitter');
  
 var client = new Twitter(twitterKeys);
 
+var client2 = new Twitter(twitterKeys);
+
+/*
 var params = {screen_name: 'nodejs'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
     console.log(tweets[0].text);
   }
 });
+*/
+
+// TEST THIS, THE FOLLOWING, OUT **** 5:34pm
+
+
+var params = {screen_name: 'iosbigfaced2'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets[0].text); // brings back FIRST tweet
+ //   console.log(tweets[1].text);
+ //   console.log(tweets[2].text);
+ //   console.log(tweets[3].text); // throws an error, ooh yeaah lol  --because there is no 4th tweet. Index starts at 0
+  }
+});
+				// 12:38am */				// remember errors? ?
+
+//. --------------------------- BEGIN, NEWER CODE, ADDING AN ADDITIONAL 'GET' REQUEST
+client.get('favorites/list', function(error, tweets, response) {
+  if(error) throw error;
+  console.log(tweets);  // The favorites. 
+ //6:27pm take out this one --> console.log(response);  // Raw response object. 
+});
+
+
+
+// how do I use this?
+GET https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=2
+
+client2.get()
+
+
+
+
+// ---------------------------------------------------------- BEGIN, NEW CODE
+
+// var twitterTimeline = require('./keys.js');
+
+// so comment this out, 12:46am --> var twitterTimeline = require('twitter-timeline-middleware'); // this is the wrong one
+
+
+
+ 
+// app.use('/timeline/:iosbigfaced2', twitterTimeline(twitterCredentials));
+
+
+
